e6a293aed22f40e48ae0e521b3a0e9aaa5f961c8
Corr79 <corr1@smartweb.augustatech.edu>
2026-02-26 20:52:34 -0500

    Applied exercise 5-1 updates to the code

--- InvoiceTotal/frmInvoiceTotal.cs
+++ InvoiceTotal/frmInvoiceTotal.cs
@@ -13,26 +13,69 @@ namespace InvoiceTotal
             decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
             decimal discountPct = 0m;
 
-            if (customerType == "R")
+            //if (customerType == "R" || customerType == "r")
+            //{
+            //    if (subtotal < 100)
+            //        discountPct = .0m;
+            //    else if (subtotal >= 100 && subtotal < 250)
+            //        discountPct = .1m;
+            //    else if (subtotal >= 250 && subtotal < 500)
+            //        discountPct = .25m;
+            //    else if (subtotal >= 500)
+            //        discountPct = .3m;
+            //}
+            //else if (customerType == "C" || customerType == "c")
+            //{
+            //    discountPct = .2m;
+            //}
+            //else if (customerType == "T" || customerType == "t")
+            //{
+            //    if (subtotal < 500)
+            //        discountPct = .4m;
+            //    else if (subtotal >= 500)
+            //        discountPct = .5m;
+            //}
+            //else
+            //{
+            //    discountPct = .1m;
+            //}
+
+            //switch (customerType)
+            //{
+            //    case "R":
+            //        if (subtotal < 100)
+            //            discountPct = .0m;
+            //        else if (subtotal >= 100 && subtotal < 250)
+            //            discountPct = .1m;
+            //        else if (subtotal >= 250 && subtotal < 500)
+            //            discountPct = .25m;
+            //        else if (subtotal >= 500)
+            //            discountPct = .3m;
+            //        break;
+
+            //    case "C":
+            //        discountPct = .2m;
+            //        break;
+
+            //    case "T":
+            //        if (subtotal < 500)
+            //            discountPct = .4m;
+            //        else if (subtotal >= 500)
+            //            discountPct = .5m;
+            //        break;
+
+            //    default:
+            //        discountPct = .1m;
+            //        break;
+            //}
+
+            discountPct = customerType switch
             {
-                if (subtotal < 100)
-                    discountPct = .0m;
-                else if (subtotal >= 100 && subtotal < 250)
-                    discountPct = .1m;
-                else if (subtotal >= 250)
-                    discountPct = .25m;
-            }
-            else if (customerType == "C")
-            {
-                if (subtotal < 250)
-                    discountPct = .2m;
-                else
-                    discountPct = .3m;
-            }
-            else
-            {
-                discountPct = .4m;
-            }
+                "R" => .3m,
+                "C" => .2m,
+                "T" => .5m,
+                _ => .1m
+            };
 
             decimal discountAmt = subtotal * discountPct;
             decimal invoiceTotal = subtotal - discountAmt;
